0d80c3926673c239e006c4e1aad4975410978db4
user <user@mail.ru>
2024-07-28 10:04:42 +0300

    test updated

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+from praktikum.ingredient import Ingredient
 
 
 class Data:
@@ -10,4 +11,20 @@ class Data:
     INGREDIENT_2_NAME = 'Ingredient_2'
     INGREDIENT_2_PRICE = 50
     INGREDIENT_2_TYPE = INGREDIENT_TYPE_FILLING
+
+class TestConfig:
+    BUNS = [
+        {"name": "black bun", "price": 100},
+        {"name": "white bun", "price": 200},
+        {"name": "red bun", "price": 300},
+    ]
+    
+    INGREDIENTS = [
+        {"type": INGREDIENT_TYPE_SAUCE, "name": "hot sauce", "price": 100},
+        {"type": INGREDIENT_TYPE_SAUCE, "name": "sour cream", "price": 200},
+        {"type": INGREDIENT_TYPE_SAUCE, "name": "chili sauce", "price": 300},
+        {"type": INGREDIENT_TYPE_FILLING, "name": "cutlet", "price": 100},
+        {"type": INGREDIENT_TYPE_FILLING, "name": "dinosaur", "price": 200},
+        {"type": INGREDIENT_TYPE_FILLING, "name": "sausage", "price": 300},
+    ]
 
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,48 +1,25 @@
 from praktikum.bun import Bun
-from praktikum.ingredient import Ingredient
-from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 from praktikum.database import Database
+from data import TestConfig
 
 class TestDatabase:
     def test_available_buns(self):
         db = Database()
         buns = db.available_buns()
 
-        assert len(buns) == 3
+        assert len(buns) == len(TestConfig.BUNS)
 
-        assert buns[0].name == "black bun"
-        assert buns[0].price == 100
-        assert buns[1].name == "white bun"
-        assert buns[1].price == 200
-        assert buns[2].name == "red bun"
-        assert buns[2].price == 300
+        for i, bun in enumerate(buns):
+            assert bun.name == TestConfig.BUNS[i]["name"]
+            assert bun.price == TestConfig.BUNS[i]["price"]
 
     def test_available_ingredients(self):
         db = Database()
         ingredients = db.available_ingredients()
 
-        assert len(ingredients) == 6
+        assert len(ingredients) == len(TestConfig.INGREDIENTS)
 
-        assert ingredients[0].get_type() == INGREDIENT_TYPE_SAUCE
-        assert ingredients[0].get_name() == "hot sauce"
-        assert ingredients[0].get_price() == 100
-
-        assert ingredients[1].get_type() == INGREDIENT_TYPE_SAUCE
-        assert ingredients[1].get_name() == "sour cream"
-        assert ingredients[1].get_price() == 200
-
-        assert ingredients[2].get_type() == INGREDIENT_TYPE_SAUCE
-        assert ingredients[2].get_name() == "chili sauce"
-        assert ingredients[2].get_price() == 300
-
-        assert ingredients[3].get_type() == INGREDIENT_TYPE_FILLING
-        assert ingredients[3].get_name() == "cutlet"
-        assert ingredients[3].get_price() == 100
-
-        assert ingredients[4].get_type() == INGREDIENT_TYPE_FILLING
-        assert ingredients[4].get_name() == "dinosaur"
-        assert ingredients[4].get_price() == 200
-
-        assert ingredients[5].get_type() == INGREDIENT_TYPE_FILLING
-        assert ingredients[5].get_name() == "sausage"
-        assert ingredients[5].get_price() == 300
+        for i, ingredient in enumerate(ingredients):
+            assert ingredient.get_type() == TestConfig.INGREDIENTS[i]["type"]
+            assert ingredient.get_name() == TestConfig.INGREDIENTS[i]["name"]
+            assert ingredient.get_price() == TestConfig.INGREDIENTS[i]["price"]
